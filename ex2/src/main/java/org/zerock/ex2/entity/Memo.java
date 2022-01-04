82127476972f04bbc010e8ff2dac26cf59d0346d
windguy1999 <windguy1999@gmail.com>
2022-01-05 00:08:14 +0900

    처음 올림 v0001

---
+++ ex2/src/main/java/org/zerock/ex2/entity/Memo.java
@@ -0,0 +1,20 @@
+package org.zerock.ex2.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tbl_memo")
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) private Long mno;
+
+    @Column(length = 200, nullable = false) private String memoText;
+    // @Column(columnDefinition = "varchar(255) default 'Yes'")  기본값 설정
+}
